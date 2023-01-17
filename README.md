dc631da1641770af9f6c6c6c209b470856969655
Anthony Pontecorvo <antpont80@gmail.com>
2023-01-17 14:17:56 -0500

    Added comments to js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # random-password-generator
-A password generator that chooses a secure and random password using JavaScript.
+
+
+## A password generator that chooses a secure and random password using JavaScript.
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// Assignment Code
+// defining global variables
 var generateBtn = document.querySelector("#generate");
-
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var lowercase = "abcdefghijklmnopqrstuvwxyz"
 var numbers = "1234567890"
@@ -14,17 +13,22 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// defining generatepassword function
 function generatePassword() {
+  // defining variables within the scope of this function
   var password = ''
   var tempCharacters = ''
 
+  // Setting a prompt for the password length variable
   var pwLength = prompt("Between 8 and 128 how many charaters would you like your password to contain?")
 
+  // If your password is less than 8 or more than 128 you will be prompted to choose between 8 and 128, then asked again.
   while (pwLength < 8 || pwLength > 128) {
     alert('Your password must be between 8 and 128 characters')
     pwLength = prompt("Between 8 and 128 how many charaters would you like your password to contain?")
   }
 
+  // It will ask if you want uppercase letters if yes is the answer we call on the uppercase variable same goes for lowercase, numbers, and special characters.
   var wantsUp = confirm("do you want uppercase letters?")
   if (wantsUp) {
     tempCharacters += uppercase
@@ -49,6 +53,7 @@ function generatePassword() {
     console.log('temoCharacters = ', tempCharacters)
   }
 
+  // If you choose nothing it will alert you that you need to choose one and re ask.
   while (!wantsUp && !wantsLower && !wantsNumber && !wantsSpecial) {
     alert('You must choose at least one character')
 
@@ -77,10 +82,12 @@ function generatePassword() {
     }
   }
 
+  // This is setting it up so that we can generate the random password by using a for loop to keep adding a random character until the variable is no longer less than the pwlength
   for (var i = 0; i < pwLength; i++) {
     password += tempCharacters.charAt(Math.floor(Math.random() * tempCharacters.length))
   }
 
+  // This is returning out a password back to the user
   return password
 }
 
